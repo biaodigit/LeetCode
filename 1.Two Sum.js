8054b46adbea1131ab0416006624fb9d5adce66e
luowendi <luowendi@umier.com.cn>
2018-01-23 18:11:25 +0800

    add preorder traversal

---
+++ 1.Two Sum.js
@@ -0,0 +1,15 @@
+const twoSum = function (nums, target) {
+    // 从无序数组中找到两个值，使两个值之和等于target
+    /**
+     * 查找表
+     * @type {Map}
+     */
+    let m = new Map(),
+        len = nums.length;
+    for (let i = 0; i < len; i++) {
+        if (m.has(nums[i])) {
+            return [m.get(nums[i]), i]
+        }
+        m.set((target - nums[i]), i)
+    }
+};
