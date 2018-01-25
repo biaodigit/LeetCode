2f89dd661bdbb176dfde3c4c1506f6b4b53ea33b
luowendi <luowendi@umier.com.cn>
2018-01-25 18:56:22 +0800

    add 19.test

---
+++ 19.Remove Nth Node From End of List.js
@@ -0,0 +1,24 @@
+const removeNthFromEnd = function (head, n) {
+    if (n <= 0 || !head) {
+        return
+    }
+    let dummyHead = new ListNode(null)
+    dummyHead.next = head
+    let p = dummyHead, q = dummyHead;
+    while (n--) {
+        p = p.next
+        if (!p) {
+            return null
+        }
+    }
+    while (p.next) {
+        p = p.next
+        q = q.next
+    }
+    let delNode = q.next
+    q.next = delNode.next
+    delNode = null
+    let retNode = dummyHead.next
+    dummyHead = null
+    return retNode
+};
