b6dd4bb4b95998067b233d769107b65cade49128
luowendi <luowendi@umier.com.cn>
2018-01-25 18:56:43 +0800

    add 102.test 107.test

---
+++ 107.Binary Tree Level Order Traversal II.js
@@ -0,0 +1,20 @@
+const levelOrderBottom = function(root) {
+    if(!root){
+        return []
+    }
+    let res = [],
+        queue = [];
+    queue.push(root)
+    while(queue.length){
+        let size = queue.length,
+            temp = [];
+        for(let i=0;i<size;i++){
+            let node = queue.shift()
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+            temp.push(node.val)
+        }
+        res.push(temp)
+    }
+    return res.reverse()
+};
