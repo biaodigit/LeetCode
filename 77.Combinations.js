1dccc4ced023753cce2d8ac37ac9b48c66dcdecf
luowendi <luowendi@umier.com.cn>
2018-02-03 00:16:40 +0800

    add 77.test

---
+++ 77.Combinations.js
@@ -0,0 +1,18 @@
+let combine = function (n, k) {
+    if (n <= 0 || k <= 0 || k > n) return []
+    let res = []
+    let generateCombination = function (n, k, start, arr) {
+        if (arr.length === k) {
+            res.push(arr.slice(0))
+            return
+        }
+        for (let i = start; i <= n - (k - arr.length) + 1; i++) {
+            arr.push(i)
+            generateCombination(n, k, i + 1, arr)
+            arr.pop()
+        }
+        return
+    }
+    generateCombination(n, k, 1, [])
+    return res
+};
