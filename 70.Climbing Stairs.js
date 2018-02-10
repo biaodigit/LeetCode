b3aad27ba2572a4107a45000ea8fa582ca404b2d
luowendi <luowendi@umier.com.cn>
2018-02-10 23:52:31 +0800

    update

--- 70.Climbing Stairs.js
+++ 70.Climbing Stairs.js
@@ -1,11 +1,22 @@
 const climbStairs = (n) => {
-    if (n === 0 || n === 1) return n
+    if (n === 0 || n === 1 || n === 2) return n
 
-    let a = 1, b = 1, result;
-    for (let i = 2; i <= n; i++) {
+    let a = 1, b = 2, result;
+    for (let i = 3; i <= n; i++) {
         result = a + b
         a = b
         b = result
     }
     return result
 };
+
+// const climbStairs = (n) => {
+//     if (n === 0 || n === 1) return n
+//
+//     const memo = []
+//     memo[1] = 1, memo[2] = 2
+//     for (let i = 3; i <= n; i++) {
+//         memo[i] = memo[i - 1] + memo[i - 2]
+//     }
+//     return memo[n]
+// };
