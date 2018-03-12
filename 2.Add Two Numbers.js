aa6fb0d398fb000d5387aa4eaa109e5faf6c4148
luowendi <luowendi@umier.com.cn>
2018-03-12 12:18:45 +0800

    add Add Two Numbers

---
+++ 2.Add Two Numbers.js
@@ -0,0 +1,25 @@
+const addTwoNumbers = function (l1, l2) {
+    let list = new ListNode(0),
+        head = list,
+        carry = 0,
+        sum = 0;
+    while (l1 !== null || l2 !== null || sum > 0) {
+        if (l1) {
+            sum += l1.val
+            l1 = l1.next
+        }
+        if (l2) {
+            sum += l2.val
+            l2 = l2.next
+        }
+        if (sum >= 10) {
+            carry = 1
+            sum -= 10
+        }
+        head.next = new ListNode(sum)
+        head = head.next
+        sum = carry
+        carry = 0
+    }
+    return list.next
+};
