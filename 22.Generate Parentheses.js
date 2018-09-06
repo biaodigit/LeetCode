8a2c42482eda61e8fe7b100e9daa589df1a6035f
luowendi <15622285015@163.com>
2018-09-06 23:33:07 +0800

    add Generate Parentheses

---
+++ 22.Generate Parentheses.js
@@ -0,0 +1,13 @@
+const generateParenthesis = (n) => {
+    let res = [];
+    generate('', n, n, res);
+    return res;
+};
+
+const generate = (str, l, r, res) => {
+    // if(l > r) return;
+    if (!l && !r) res.push(str);
+    if (l) generate(str + '(', l - 1, r, res);
+    if (r) generate(str + ')', l, r - 1, res);
+};
+
