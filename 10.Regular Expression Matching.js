c44fab7dec312ee94eca41f45306f6786fc4931c
luowendi <15622285015@163.com>
2018-10-07 11:56:14 +0800

    update

--- 10.Regular Expression Matching.js
+++ 10.Regular Expression Matching.js
@@ -1,4 +1,33 @@
 let isMatch = function (s, p) {
-    let reg = new RegExp(`^${p}$`);
-    return reg.test(s)
+    let dp = Array(s.length + 1);
+    for (let i = 0; i < dp.length; i++) {
+        dp[i] = Array(p.length + 1).fill(false)
+    }
+    dp[0][0] = true;
+    for (let i = 1; i < p.length; i++) {
+        if (p.charAt(i) === "*" && dp[0][i - 1]) {
+            dp[0][i + 1] = true
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        for (let j = 0; j < p.length; j++) {
+            if (p.charAt(j) === '.') {
+                dp[i + 1][j + 1] = dp[i][j]
+            }
+
+            if (p.charAt(j) === s.charAt(i)) {
+                dp[i + 1][j + 1] = dp[i][j]
+            }
+
+            if (p.charAt(j) === '*') {
+                if (p.charAt(j - 1) !== s.charAt(i) && p.charAt(j - 1) !== '.') {
+                    dp[i + 1][j + 1] = dp[i + 1][j - 1]
+                } else {
+                    dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1])
+                }
+            }
+        }
+    }
+    return dp[s.length][p.length]
 };
--- 9.Palindrome Number.js
+++ 9.Palindrome Number.js
@@ -1,4 +1,11 @@
-let isPalindrome = function(x) {
-    let y = parseInt(x.toString().split('').reverse().join(''));
-    return x === y
+let isPalindrome = function (x) {
+    if (x < 0 || x % 10 === 0 && x !== 0) return false;
+    let temp = x;
+    let reverse = 0;
+    while (temp > 0) {
+        reverse = reverse * 10 + temp % 10;
+        temp = Math.floor(temp / 10);
+    }
+
+    return x === reverse
 };
