2f4a28577028484e6c7c413ddd9335274d901fe6
luowendi <15622285015@163.com>
2018-10-19 10:52:33 +0800

    rewrite String to Integer(atoi)

--- 8.String to Integer (atoi).js
+++ 8.String to Integer (atoi).js
@@ -1,3 +1,29 @@
-let myAtoi = function(str) {
-    return Math.max(Math.min(parseInt(str) || 0, 2147483647), -2147483648)
+let myAtoi = function (str) {
+    let i = 0,
+        res = 0,
+        isNegative = false,
+        code;
+
+    while (str[i] === ' ') {
+        i++
+    }
+
+
+    if (str[i] === '+' || str[i] === '-') {
+        isNegative = str[i] === '-';
+        i++
+    }
+
+    while (i < str.length) {
+        code = str.charCodeAt(i) - 48;
+        if (code < 0 || code > 9) break;
+
+        res *= 10;
+        res += code;
+        i++
+    }
+    if (isNegative) {
+        res = -res
+    }
+    return Math.max(Math.min(res, 2147483647), -2147483648)
 };
