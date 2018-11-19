6c4b660f29fc0763a26318c291c73c64d1b3e04b
luowendi <15622285015@163.com>
2018-11-19 17:41:40 +0800

    add Permutation Sequence

---
+++ 60.Permutation Sequence.js
@@ -0,0 +1,19 @@
+let getPermutation = function (n, k) {
+    //n = 4, k = 9 => 2314
+    let ret = [],
+        arr = [],
+        pos = k - 1,
+        sum;
+
+    for (let i = 1; i <= n; i++) {
+        arr.push(i)
+    }
+    sum = arr.reduce((pre, curr) => pre * curr)
+
+    for (let i = n; i >= 1; i--) {
+        sum /= i;
+        ret.push(arr.splice(Math.floor(pos / sum), 1)[0])
+        pos %= sum
+    }
+    return ret.join('')
+};
