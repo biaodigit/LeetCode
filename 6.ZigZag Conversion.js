14c6d5a2fae28fa2271a2cbd893fe12452e1fcd7
luowendi <15622285015@163.com>
2019-07-18 22:40:29 +0800

    update ZigZag Conversion

--- 6.ZigZag Conversion.js
+++ 6.ZigZag Conversion.js
@@ -1,14 +1,17 @@
-let convert = function (s, nRows) {
-    if (nRows <= 1) return s
-    let str = "",
-        distance = nRows * 2 - 2;
-    for (let i = 0; i < nRows; i++) {
-        for (let j = i; j < s.length; j += distance) {
-            str += s[j]
-            let temp = j + distance - i * 2
-            if(i !== 0 && i !== nRows - 1 && temp < s.length){
-                str += s[temp]
-            }
-        }
+let convert = (s, nRows) => {
+    if (nRows <= 1) return s;
+    let rows = Array(Math.min(nRows, s.length)).fill(''),
+        goingDown = false,
+        curRow = 0,
+        res = '';
+    for (let i = 0, len = s.length; i < len; i++) {
+        rows[curRow] += s.charAt(i);
+        if (curRow === 0 || curRow === nRows - 1) goingDown = !goingDown;
+        curRow += goingDown ? 1 : -1
     }
+    for (let str of rows) {
+        res += str
+    }
+
+    return res
 };
