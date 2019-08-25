120c7cd3adb1e341cebb4d536e495d8351d54b5f
luowendi <15622285015@163.com>
2019-08-25 19:19:40 +0800

    update Recover Binary Search Tree

--- 99.Recover Binary Search Tree.js
+++ 99.Recover Binary Search Tree.js
@@ -1,25 +1,41 @@
 let recoverTree = function (root) {
     let curr = root,
-        stack = [],
         p1 = null,
         p2 = null,
-        prev = null;
+        prev = null,
+        temp = null;
 
-    while (curr || stack.length) {
-        while (curr) {
-            stack.push(curr);
-            curr = curr.left
+    while (curr) {
+        if (curr.left) {
+            temp = curr.left;
+            while (temp.right && temp.right !== curr) {
+                temp = temp.right
+            }
+            if (!temp.right) {
+                temp.right = curr;
+                curr = curr.left
+            } else {
+              if(prev && curr.val < prev.val){
+                  p1 = curr;
+                  if(!p2) p2 = prev
+              }
+              temp.right = null;
+              prev = curr;
+              curr = curr.right
+            }
+        } else {
+            if (prev && curr.val < prev.val) {
+                p1 = curr;
+                if (!p2) p2 = prev
+            }
+            prev = curr;
+            curr = curr.right
         }
-        curr = stack.pop();
-        if (prev && curr.val < prev.val) {
-            p1 = curr;
-            if (!p2) p2 = prev
-        }
-        prev = curr;
-        curr = curr.right
     }
 
-    let temp = p1.val;
+
+    temp = p1.val;
     p1.val = p2.val;
     p2.val = temp
 };
+
