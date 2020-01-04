cb2bb5b43daef46c8f17a592f516499bba63932c
biaodigit <15622285015@163.com>
2020-01-04 19:08:30 +0800

    update: Pascal's Triangle II

--- library/119.Pascal's Triangle II.js
+++ library/119.Pascal's Triangle II.js
@@ -1,12 +1,10 @@
-let getRow = function(rowIndex) {
-    let dp = [];
-    for (let i = 0; i <= rowIndex; i++) {
-        dp[i] = [];
-        dp[i][0] = 1;
-        for (let j = 1; j < i; j++) {
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+let getRow = function (rowIndex) {
+    let res = [1];
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = res.length - 1; j > 0; j--) {
+            res[j] = res[j] + res[j - 1]
         }
-        dp[i][i] = 1
+        res.push(1)
     }
-    return dp[rowIndex]
+    return res
 };
