0cdb4f327cbc8615d11c065bc1b66df2b0696fb4
biaodigit <15622285015@163.com>
2020-05-06 21:57:17 +0800

    rewrite Surrounded Regions of BFS

--- library/130.Surrounded Regions.js
+++ library/130.Surrounded Regions.js
@@ -1,50 +1,57 @@
+const d = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+let bfs = (board, visited, x, y, m, n) => {
+    let queue = [{ x, y }]
+    visited[x][y] = true
+    while (queue.length) {
+        let curr = queue.shift()
+        for (let i = 0; i < 4; i++) {
+            let newX = curr.x + d[i][0],
+                newY = curr.y + d[i][1];
+            if (inArea(newX, newY, m, n) && !visited[newX][newY] && board[newX][newY] === 'O') {
+                queue.push({ x: newX, y: newY })
+                visited[newX][newY] = true
+            }
+        }
+    }
+}
+
+let inArea = (x, y, m, n) => {
+    return x >= 0 && x < m && y >= 0 && y < n
+}
+
+let isEdge = (x, y, m, n) => {
+    return x === 0 || x === m - 1 || y === 0 || y === n - 1
+}
+
 let solve = (board) => {
     let m = board.length;
     if (!m) return
     let n = board[0].length;
     if (!n) return
 
-    const d = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-
     let visited = Array(m).fill(null).map(p => Array(n).fill(false))
-
-
-    let inArea = (i, j) => {
-        return i >= 0 && i < m && j >= 0 && j < n
-    }
-
-    let search = (x, y) => {
-        visited[x][y] = true
-        for (let i = 0; i < 4; i++) {
-            let newX = x + d[i][0],
-                newY = y + d[i][1]
-            if (inArea(newX, newY) && board[newX][newY] === 'O' && !visited[newX][newY]) {
-                search(newX, newY)
-            }
-        }
-    }
-
     for (let i = 1; i < n - 1; i++) {
         if (board[0][i] === 'O') {
-            search(0, i)
+            bfs(board, visited, 0, i, m, n)
         }
     }
 
     for (let i = 1; i < m - 1; i++) {
         if (board[i][0] === 'O') {
-            search(i, 0)
+            bfs(board, visited, i, 0, m, n)
         }
     }
 
     for (let i = 1; i < n - 1; i++) {
         if (board[m - 1][i] === 'O') {
-            search(m - 1, i)
+            bfs(board, visited, m - 1, i, m, n)
         }
     }
 
     for (let i = 1; i < m - 1; i++) {
         if (board[i][n - 1] === 'O') {
-            search(i, n - 1)
+            bfs(board, visited, i, n - 1, m, n)
         }
     }
 
